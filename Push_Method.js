9693439e77f2495f1f8c4423e114b1a94b727be3
Md Mohiuddin Murad <104640147+muradmy00@users.noreply.github.com>
2025-11-22 20:34:11 +0600

    array index and value

--- Push_Method.js
+++ Push_Method.js
@@ -1,4 +1,4 @@
-let array = [10, 20, 30]
+let array = [20,10,30]
 
 let newArray = array.push(40);
 
@@ -6,3 +6,21 @@ let newArray = array.push(40);
 
 // console.log(newArray); ==> 4 because push method return a array length
 
+
+
+let index = array.indexOf(20);
+
+if (index !== -1) {
+    console.log("ache");
+    console.log("Index =", index);
+} else {
+    console.log("nai");
+}
+
+
+
+// if (array.includes(20)) {
+//     console.log("ache");
+// } else {
+//     console.log("nai");
+// }
