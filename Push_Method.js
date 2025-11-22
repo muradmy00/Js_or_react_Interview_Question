316118832947658ba511ae55b820d4185d7be19a
Md Mohiuddin Murad <104640147+muradmy00@users.noreply.github.com>
2025-11-22 14:42:34 +0600

    push method use to array operation

---
+++ Push_Method.js
@@ -0,0 +1,8 @@
+let array = [10, 20, 30]
+
+let newArray = array.push(40);
+
+// console.log(array); ==> [10, 20, 30, 40]
+
+// console.log(newArray); ==> 4 because push method return a array length
+
