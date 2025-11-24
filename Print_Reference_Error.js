6fac923095e2953d5521134039c0d3d0659a0c63
Mohiuddin Murad <murad.my00@gmail.com>
2025-11-24 21:10:09 +0600

    check push

--- Print_Reference_Error.js
+++ Print_Reference_Error.js
@@ -10,7 +10,6 @@ function demo() {
 
 demo();
 
-
 // Solution
 // IIFE function means Immediately Invoked Function Expression
 // it's work: ei function ke call kora lage na automatically call hoi () er jonno
